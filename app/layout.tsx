f43c568f8bd145b4ca9d102ae63ef8de9726f0fa
G. Willson <greg@biomassiv.es>
2024-08-10 17:42:57 -0600

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,9 +3,9 @@ import './globals.css';
 import { Analytics } from '@vercel/analytics/react';
 
 export const metadata = {
-  title: 'Next.js App Router + NextAuth + Tailwind CSS',
+  title: 'worldbridger one',
   description:
-    'A user admin dashboard configured with Next.js, Postgres, NextAuth, Tailwind CSS, TypeScript, and Prettier.'
+    'A user assisting service for ecocity/ exotopia terrestrial intefacing.'
 };
 
 export default function RootLayout({
